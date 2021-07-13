8868e1615e0ffcd02c2dfd3c5a7ae3f84357fa7f
edenhailu333@gmail.com <EDENhailu21>
2021-07-13 09:03:33 -0500

    set up made

---
+++ react-gallary/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import Nav from './Components/Nav';
+import PhotoContainer from './Components/PhotoContainer';
+import NotFound from
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default App;
---
+++ react-gallary/src/Components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+class  extends Component {
+    render() { 
+        return ( 
+        <nav className="main-nav">
+        <ul>
+          <li><a href='#'>Cats</a></li>
+          <li><a href='#'>Dogs</a></li>
+          <li><a href='#'>Computers</a></li>
+        </ul>
+      </nav>
+         );
+    }
+}
+ 
+export default Nav;
